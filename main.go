3ecb1b61defbb38df760b0ef62e64f0ac46449d3
RockyFu267 <453618045@qq.com>
2025-02-04 16:37:37 +0800

    添加4张卡背

--- main.go
+++ main.go
@@ -55,15 +55,58 @@ func main() {
 		// 例如打印日志，当前仅作示例
 		println("长方形被点击了！")
 	})
-
+	// 创建一个可点击的按钮来模拟长方形
+	clickableRectflop02 := widget.NewButton("", func() {
+		// 这里可以添加按钮点击后的处理逻辑
+		// 例如打印日志，当前仅作示例
+		println("长方形被点击了！")
+	})
+	// 创建一个可点击的按钮来模拟长方形
+	clickableRectflop03 := widget.NewButton("", func() {
+		// 这里可以添加按钮点击后的处理逻辑
+		// 例如打印日志，当前仅作示例
+		println("长方形被点击了！")
+	})
+	// 创建一个可点击的按钮来模拟长方形
+	clickableRectflop04 := widget.NewButton("", func() {
+		// 这里可以添加按钮点击后的处理逻辑
+		// 例如打印日志，当前仅作示例
+		println("长方形被点击了！")
+	})
+	// 创建一个可点击的按钮来模拟长方形
+	clickableRectflop05 := widget.NewButton("", func() {
+		// 这里可以添加按钮点击后的处理逻辑
+		// 例如打印日志，当前仅作示例
+		println("长方形被点击了！")
+	})
 	// 设置按钮的大小为 50x90
 	clickableRectflop01.Resize(fyne.NewSize(50, 90))
+	// 设置按钮的大小为 50x90
+	clickableRectflop02.Resize(fyne.NewSize(50, 90))
+	// 设置按钮的大小为 50x90
+	clickableRectflop03.Resize(fyne.NewSize(50, 90))
+	// 设置按钮的大小为 50x90
+	clickableRectflop04.Resize(fyne.NewSize(50, 90))
+	// 设置按钮的大小为 50x90
+	clickableRectflop05.Resize(fyne.NewSize(50, 90))
 
 	// 创建一个容器用于绝对定位
 	positionContainer := container.NewWithoutLayout()
 	positionContainer.Add(clickableRectflop01)
+	positionContainer.Add(clickableRectflop02)
+	positionContainer.Add(clickableRectflop03)
+	positionContainer.Add(clickableRectflop04)
+	positionContainer.Add(clickableRectflop05)
 	// 将按钮移动到左上角 (0, 0) 的位置
-	clickableRectflop01.Move(fyne.NewPos(0, 0))
+	clickableRectflop01.Move(fyne.NewPos(0, 30))
+	// 将按钮移动到左上角 (55, 0) 的位置
+	clickableRectflop02.Move(fyne.NewPos(55, 30))
+	// 将按钮移动到左上角 (110, 0) 的位置
+	clickableRectflop03.Move(fyne.NewPos(110, 30))
+	// 将按钮移动到左上角 (165, 0) 的位置
+	clickableRectflop04.Move(fyne.NewPos(165, 30))
+	// 将按钮移动到左上角 (220, 0) 的位置
+	clickableRectflop05.Move(fyne.NewPos(220, 30))
 
 	// 使用 NewPadded 容器将绝对定位容器居中显示在窗口中
 	content := container.NewPadded(positionContainer)
