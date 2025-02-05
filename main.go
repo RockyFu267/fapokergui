858b9a44dc199e70865c9591c25179e5f1d60f02
RockyFu267 <453618045@qq.com>
2025-02-05 21:12:52 +0800

    01-05 重做

--- main.go
+++ main.go
@@ -74,66 +74,28 @@ func main() {
 	// 设置窗口的大小为自定义分辨率
 	w.Resize(fyne.NewSize(350, 780))
 
-	// 创建一个可点击的按钮来模拟长方形
-	clickableRectflop01 := widget.NewButton("", func() {
-		// 这里可以添加按钮点击后的处理逻辑
-		// 例如打印日志，当前仅作示例
-		println("clickableRectflop01 被点击了！")
-	})
-
-	// 创建一个可点击的按钮来模拟长方形
-	clickableRectflop02 := widget.NewButton("", func() {
-		// 这里可以添加按钮点击后的处理逻辑
-		// 例如打印日志，当前仅作示例
-		println("长方形被点击了！")
-	})
-	// 创建一个可点击的按钮来模拟长方形
-	clickableRectflop03 := widget.NewButton("", func() {
-		// 这里可以添加按钮点击后的处理逻辑
-		// 例如打印日志，当前仅作示例
-		println("长方形被点击了！")
-	})
-	// 创建一个可点击的按钮来模拟长方形
-	clickableRectflop04 := widget.NewButton("", func() {
-		// 这里可以添加按钮点击后的处理逻辑
-		// 例如打印日志，当前仅作示例
-		println("长方形被点击了！")
-	})
-	// 创建一个可点击的按钮来模拟长方形
-	clickableRectflop05 := widget.NewButton("", func() {
-		// 这里可以添加按钮点击后的处理逻辑
-		// 例如打印日志，当前仅作示例
-		println("长方形被点击了！")
-	})
-
-	// 设置按钮的大小为 50x90
-	clickableRectflop01.Resize(fyne.NewSize(50, 90))
-	// 设置按钮的大小为 50x90
-	clickableRectflop02.Resize(fyne.NewSize(50, 90))
-	// 设置按钮的大小为 50x90
-	clickableRectflop03.Resize(fyne.NewSize(50, 90))
-	// 设置按钮的大小为 50x90
-	clickableRectflop04.Resize(fyne.NewSize(50, 90))
-	// 设置按钮的大小为 50x90
-	clickableRectflop05.Resize(fyne.NewSize(50, 90))
-
 	// 创建一个容器用于绝对定位
 	positionContainer := container.NewWithoutLayout()
-	positionContainer.Add(clickableRectflop01)
-	positionContainer.Add(clickableRectflop02)
-	positionContainer.Add(clickableRectflop03)
-	positionContainer.Add(clickableRectflop04)
-	positionContainer.Add(clickableRectflop05)
-	// 将按钮移动到左上角 (0, 30) 的位置
-	clickableRectflop01.Move(fyne.NewPos(0, 30))
-	// 将按钮移动到左上角 (55, 30) 的位置
-	clickableRectflop02.Move(fyne.NewPos(55, 30))
-	// 将按钮移动到左上角 (110, 30) 的位置
-	clickableRectflop03.Move(fyne.NewPos(110, 30))
-	// 将按钮移动到左上角 (165, 30) 的位置
-	clickableRectflop04.Move(fyne.NewPos(165, 30))
-	// 将按钮移动到左上角 (220, 30) 的位置
-	clickableRectflop05.Move(fyne.NewPos(220, 30))
+	// 预定义按钮的位置
+	flopPositions := []fyne.Position{
+		fyne.NewPos(0, 30),
+		fyne.NewPos(55, 30),
+		fyne.NewPos(110, 30),
+		fyne.NewPos(165, 30),
+		fyne.NewPos(220, 30),
+	}
+
+	var flopButtons []*widget.Button
+
+	for _, pos := range flopPositions {
+		btn := widget.NewButton("", func() {
+			println("长方形被点击了！")
+		})
+		btn.Resize(fyne.NewSize(50, 90))
+		btn.Move(pos)
+		flopButtons = append(flopButtons, btn)
+		positionContainer.Add(btn)
+	}
 
 	// 记录新增按钮的行数
 	newRowCount := 0
